7a729c3c28a142ac5dc37234163d2cf1ddba0813
unknown <personal.abusaid@gmail.com>
2023-03-28 13:00:45 +0600

    Done first day

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,11 +4,16 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [Cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const handleAddToCart = (product) => {
+        const newCart = [...Cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='Shop-container'>
             <div className="product-container">
@@ -16,12 +21,15 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
+
                     ></Product>)
                 }
 
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
+                <p>Selected items:{Cart.length}</p>
             </div>
 
         </div>
